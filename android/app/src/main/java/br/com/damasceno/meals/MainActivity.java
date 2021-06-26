bb897494e4bb295dfcc1bf118dbd7ae97cd577f1
viniciusdds <viniciusdamasceno180@hotmail.com>
2021-06-26 20:14:16 -0300

    aula 182

---
+++ android/app/src/main/java/br/com/damasceno/meals/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.damasceno.meals;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/components/category_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+
+  final Category category;
+
+  const CategoryItem(this.category);
+
+  void _selectCategory(BuildContext context){
+    Navigator.of(context).push(
+        MaterialPageRoute(
+            builder: (_){
+              return CategoriesMealsScreen(category);
+            }
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).primaryColor,
+      onTap: () => _selectCategory(context),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+          ),
+        ),
+      ),
+    );
+  }
+}
